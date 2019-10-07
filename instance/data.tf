d4d190ff134531e81858c4e4a5361679177f57ef
Henning Häcker <henning.haecker+github.com@gmail.com>
2019-10-07 19:01:10 +0200

    add setup with remote state, update Readme

--- instance/data.tf
+++ instance/data.tf
@@ -2,7 +2,6 @@ data "openstack_images_image_v2" "ubuntu" {
   properties {
     ssh-user               = "ubuntu"
     docker_version         = "18.09.2"
-    docker_compose_version = "1.23.2"
   }
 }
 
@@ -11,9 +10,5 @@ data "template_file" "userdata" {
 
   vars {
     ADMIN_PASSWORD = "${var.ADMIN_PASSWORD}"
-    SERVERNAME = "${var.SERVERNAME}"
-    CPANEL_USERNAME       = "${var.CPANEL_USERNAME}"
-    CPANEL_PASSWORD       = "${var.CPANEL_PASSWORD}"
-    HOSTNAME = "${var.HOSTNAME}"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable username {
+  type = "string"
+}
+
+variable password {
+  type = "string"
+}
+
+variable domain_name {
+  # https://www.terraform.io/docs/providers/openstack/#domain_name
+  type = "string"
+}
+
+variable user_domain_name {
+  # https://www.terraform.io/docs/providers/openstack/#user_domain_name
+  type    = "string"
+  default = ""
+}
+
+variable tenant_name {
+  # https://www.terraform.io/docs/providers/openstack/#tenant_name
+  type = "string"
+}
+
+variable project_id {
+  type = "string"
+}
+
+variable auth_url {
+  type = "string"
+}
+
+variable region {
+  type    = "string"
+  default = "Karlsruhe"
+}
+
+variable "ADMIN_PASSWORD" {
+  type = "string"
+}
+
+# remote state
+# see https://www.ibm.com/cloud/blog/store-terraform-states-cloud-object-storage
+
+terraform {
+  backend "s3" {
+    bucket                      = "113e5b86ad397e6a9bf29b5db2efaf06"
+    key                         = "remote_states/ts3-terraform.tfstate"
+    region                      = "eu-de"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    endpoint                    = "s3.eu-de.cloud-object-storage.appdomain.cloud"
+  }
+}
